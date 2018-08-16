1af602bb875b759e966c931a65fa34252f6f9362
mallorymaher <mallory.maher@comcast.net>
2018-08-16 15:32:37 +0000

    routes, cities

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,25 @@ class ApplicationController < Sinatra::Base
   get '/' do
     return erb :index
   end
+  
+  post '/thankyou' do
+    "We are so glad that you chose Mae Travel Associates, #{params[:name]} to help you get the most out of your next trip. We will be in contact with you soon. <a href = #PUT OUR FINAL LINK HERE Click here "
+  end
+  
+  post '/newyork' do
+    erb :newyork
+  end
+  
+  post '/losang' do
+    erb :losang
+  end
+  
+  post '/stlouis' do
+    erb :stlouis
+  end
+
+  post '/thankyou' do
+    erb :thankyou
+  end
+  
 end
